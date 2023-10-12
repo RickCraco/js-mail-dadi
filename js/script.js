e42f15909044bf5411d983aebb3d387f338e6341
RickCraco <riccardocracolici43@gmail.com>
2023-10-12 16:07:09 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const emailList = ['username@30.com', 'pippo@pluto.it', 'giornogiovanna10@napoli.it'];
 
 const sendBtn = document.getElementById('invia');
+let flag = false;
+
 
 sendBtn.addEventListener('click', 
 
@@ -10,8 +12,13 @@ sendBtn.addEventListener('click',
         for(let i = 0; i < emailList.length; i++){
             if(userEmail === emailList[i]){
                 alert('utente trovato');
+                flag = true;
                 break;
+            }else{
+                alert('utente non trovato');
             }
         }
+
+
     }
 )
